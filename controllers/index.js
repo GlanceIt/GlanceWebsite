a52709eb332debb159030205a63f8642b21f9361
Mamadi <mamadi@gmail.com>
2015-09-23 04:22:29 +0000

    [GlanceApp-4] Added support for location search

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var geocoder = require('node-geocoder').getGeocoder('google', 'http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,24 +11,48 @@ router.get('/', function(req, res, next) {
 router.get('/spotlist', function(req, res) {
     var db = req.db;
     var collection = db.get('spotcollection');
-    collection.find({},{},function(e,docs){
+
+    // TODO: use the actual location of the user
+    var currLoc = '38 Prism Irvine CA';
+    geocoder.geocode(currLoc, function(err, result) {
+        getNearbySpots(collection, result, res);
+    });
+});
+
+/* GET Spotlist page. */
+router.get('/city/:city', function(req, res) {
+    var db = req.db;
+    var collection = db.get('spotcollection');
+    var city = req.params.city;
+
+    geocoder.geocode(city, function(err, result) {
+        getNearbySpots(collection, result, res);
+    });
+});
+
+function getNearbySpots(collection, result, res) {
+    collection.col.aggregate(
+        [
+            {$geoNear: {near: {type: "Point", coordinates: [result[0].longitude, result[0].latitude]}, distanceField: "dist.calculated", num: 10, spherical: true}}
+        ],
+        {}, function(e, docs){
         res.render('spot_list', {
             "spot_list" : docs,
             title : 'Glance | Find your favorite Spot at a glance!'
         });
     });
-});
+}
 
 /* GET Spot detail page. */
 router.get('/spots/:spot', function(req, res) {
     var db = req.db;
     var collection = db.get('spotcollection');
     var spot = req.params.spot;
-    collection.findOne({name: spot},{},function(e,docs){
-	if (e) { 
+    collection.findOne({index: spot},{},function(err,docs){
+	if (err) {
             console.log('Could not find spot ' + spot + ' in DB');
         } else { 
-            console.log('found ' + docs.name + ' in DB');
+            console.log('found ' + docs.index + ' in DB');
         }
 
         res.render('spot_detail_page', {
