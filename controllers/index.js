c96255db50531e62102a1e23d400242cafa0d04f
Mamadi <mamadi@gmail.com>
2015-09-28 08:19:54 +0000

    Use service calls in controllers to remove the DB access code from the website package

--- controllers/index.js
+++ controllers/index.js
@@ -1,62 +1,57 @@
 var express = require('express');
 var router = express.Router();
 var geocoder = require('node-geocoder').getGeocoder('google', 'http');
+var client = require('node-rest-client').Client;
+var serviceEndpoint = "http://ec2-52-89-65-158.us-west-2.compute.amazonaws.com:5000";
+
+restClient = new client();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Glance | Find your favorite Spot at a glance!' });
+    restClient.get(serviceEndpoint, function(data, response){
+        var apiResJson = JSON.parse(data.toString('utf8'));
+        var message = apiResJson.message;
+
+        res.render('index', { title: message});
+    });
 });
 
 /* GET Spotlist page. */
 router.get('/spotlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('spotcollection');
+    restClient.get(serviceEndpoint + "/spotlist", function(data, response){
+        var apiResJson = JSON.parse(data.toString('utf8'));
+        var spots = apiResJson.result;
 
-    // TODO: use the actual location of the user
-    var currLoc = '38 Prism Irvine CA';
-    geocoder.geocode(currLoc, function(err, result) {
-        getNearbySpots(collection, result, res);
-    });
-});
-
-/* GET Spotlist page. */
-router.get('/city/:city', function(req, res) {
-    var db = req.db;
-    var collection = db.get('spotcollection');
-    var city = req.params.city;
-
-    geocoder.geocode(city, function(err, cityLoc) {
-        getNearbySpots(collection, cityLoc, res);
-    });
-});
-
-function getNearbySpots(collection, cityLoc, res) {
-    collection.col.aggregate(
-        [
-            {$geoNear: {near: {type: "Point", coordinates: [cityLoc[0].longitude, cityLoc[0].latitude]}, distanceField: "dist.calculated", num: 10, spherical: true}}
-        ],
-        {}, function(e, docs){
         res.render('spot_list', {
-            "spot_list" : docs,
+            "spot_list" : spots,
             title : 'Glance | Find your favorite Spot at a glance!'
         });
     });
-}
+});
+
+/* GET City spots page. */
+router.get('/city/:city', function(req, res) {
+    var city = req.params.city;
+    restClient.get(serviceEndpoint + "/city/" + city, function(data, response){
+        var apiResJson = JSON.parse(data.toString('utf8'));
+        var citySpots = apiResJson.result;
+
+        res.render('spot_list', {
+            "spot_list" : citySpots,
+            title : 'Glance | Find your favorite Spot at a glance!'
+        });
+    });
+});
 
 /* GET Spot detail page. */
 router.get('/spots/:spot', function(req, res) {
-    var db = req.db;
-    var collection = db.get('spotcollection');
     var spot = req.params.spot;
-    collection.findOne({index: spot},{},function(err,docs){
-	if (err) {
-            console.log('Could not find spot ' + spot + ' in DB');
-        } else { 
-            console.log('found ' + docs.index + ' in DB');
-        }
+    restClient.get(serviceEndpoint + "/spots/" + spot, function(data, response){
+        var apiResJson = JSON.parse(data.toString('utf8'));
+        var spotDetails = apiResJson.result;
 
         res.render('spot_detail_page', {
-            "spot_details" : docs,
+            "spot_details" : spotDetails,
             title : spot
         });
     });
